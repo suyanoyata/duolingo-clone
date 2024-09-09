c80fe06e18cb9bcd0f63424597c0067845cb6ed4
suyanoyata <teikukuheia@gmail.com>
2024-09-10 02:58:54 +0300

    fix(tailwind): resolved content path for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./challenges/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
